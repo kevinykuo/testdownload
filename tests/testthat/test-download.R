a0956305760d59d121fe008c0c7e77821ee80b16
kevinykuo <kevinykuo@gmail.com>
2018-05-10 17:13:04 -0400

    add apache http test

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -7,6 +7,13 @@ test_that("we can download from apache.org", {
   )
 })
 
+test_that("we can download from apache.org (http)", {
+  testthat::expect_error(
+    download.file("http://apache.org", "foo.html"),
+    NA
+  )
+})
+
 test_that("we can download from github", {
   expect_error(
     download.file("https://github.com", "bar.html"),
